ab628f7f2f6a3548c3f1cee420d3003a317b08fd
italopenha <italopenha77@outlook.com>
2022-04-05 20:52:50 -0300

    @italopenha [Lista 5]

---
+++ Lista 5/IMC_V2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lista_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            float altura;
+            float peso;
+            float imc;
+
+            Console.WriteLine("Insira seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Insira sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira sua altura: ");
+            altura = (float) Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira seu peso: ");
+            peso = Convert.ToInt32(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + imc  );
+
+            if (imc < 18.5) 
+            {Console.WriteLine("Abaixo do peso");}
+
+            else if (imc < 25)
+            {Console.WriteLine("Peso normal");}
+
+            else if (imc < 30)
+            {Console.WriteLine("Sobrepeso");}
+
+            else if (imc >= 30)
+            {Console.WriteLine("Obesidade");}
+
+
+        }
+    }
+}
