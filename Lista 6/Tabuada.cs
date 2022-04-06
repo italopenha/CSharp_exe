17da5ac3c28204d5f2286b153ce351626247a9cf
italopenha <italopenha77@outlook.com>
2022-04-06 18:21:29 -0300

    @italopenha Lista 6

---
+++ Lista 6/Tabuada.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lista_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado;
+
+            Console.Write("Tabuada do: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for(int x = 1; x <= 10; x++)
+            {
+                resultado = numero * x;
+                Console.WriteLine($"{numero} x {x} = {resultado}");
+            }
+
+        }   
+    }
+}
