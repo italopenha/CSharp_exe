6353654ded0fe1a0eb9269f5477a6dc562b78b8f
italopenha <italopenha77@outlook.com>
2022-04-10 20:00:07 -0300

    @italopenha Lista 8

---
+++ Lista 8/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lista_8.src
+{
+    public class carro
+    {
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Cor { get; set; }
+
+        public int Preco { get; set; }
+        
+        public carro(string _marca, string _modelo, string _cor, int _preco)
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            Cor = _cor;
+            Preco = _preco;
+        }
+
+        public void combustivel()
+        {
+            Console.WriteLine("Etanol");
+        }
+
+        public void roda()
+        {
+            Console.WriteLine("Aro 18");
+        }
+        
+    }
+}
---
+++ Lista 8/src/carros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lista_8.src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            carro cruze = new carro("Chevrolet", "Cruze", "Azul", 70000);
+            carro lancer = new carro("Mitsubishi", "Lancer", "Branco", 50000);
+            carro onix = new carro("Chevrolet", "Onix", "Preto", 40000);
+
+            Console.WriteLine($"Carro 1 é o {cruze.Marca} {cruze.Modelo} de cor {cruze.Cor} e custa {cruze.Preco}");
+            Console.WriteLine($"Carro 2 é o {lancer.Marca} {lancer.Modelo} de cor {lancer.Cor} e custa {lancer.Preco}");
+            Console.WriteLine($"Carro 3 é o {onix.Marca} {onix.Modelo} de cor {onix.Cor} e custa {onix.Preco}");
+
+            cruze.combustivel();
+            lancer.roda();
+        }   
+    }
+}   
